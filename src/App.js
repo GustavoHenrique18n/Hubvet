05e565f9e732860305aad05ed2863449a1986bd4
Gustavo Henrique <gustavohenriquedoespirito@gmail.com>
2021-03-19 01:13:39 -0300

    iniciando

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import CardTravel from './Components/cards/Card'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <CardTravel/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/cards/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardTravel = () =>(
+
+    <div>inicial</div>
+)
+ 
+
+export default CardTravel
